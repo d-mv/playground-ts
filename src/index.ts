0af6e19f94034d3633f6d557d5fbb92767f6ab97
d-mv <d.dash.mv@gmail.com>
2020-02-23 11:17:13 +0200

    init: setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * DON'T REMOVE - required for ts-node-dev not to quit
+ *  */
+import { keeper } from './keeper'
+keeper()
+// right your code below
+
+
+
+
---
+++ src/keeper.ts
@@ -0,0 +1,3 @@
+export function keeper() {
+  setInterval(() => {}, 10000)
+}
