3753d13c49db88e26be947ec6cab79c6d16a44a2
Dmitry Melnikov <dmitry.melnikov@changehealthcare.com>
2023-10-18 16:30:33 +0100

    feat: vanilla js, bump libs

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,11 @@
  * DON'T REMOVE - required for ts-node-dev not to quit
  *  */
 import { keeper } from './keeper'
-import dayjs from 'dayjs'
-import duration from 'dayjs/plugin/duration'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
-dayjs.extend(customParseFormat)
-
-dayjs.extend(duration)
-
-import { WsService } from './socket.service'
 
 const { log } = console
 
 keeper()
-/** links */
-// ramda - https://ramdajs.com/docs/
-// closure compiler - https://closure-compiler.appspot.com/home
 
 // write your code below
-WsService.instance('http://localhost:8080')
 
-const d = '10/01/2020'
-
-const n = dayjs(new Date())
-
-log(dayjs(d, 'MM/DD/YYYY').toString())
-log(n.toString())
-log(dayjs.duration(n.diff(d)).asDays())
+log('hello')
